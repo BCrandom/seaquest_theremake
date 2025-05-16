5f58b099105f6e67f6fdf096d0cbc8fd45a0de06
BCrandom <coreabarbara306@gmail.com>
2025-05-16 16:25:08 -0400

    adicion de librerias

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
 --[[ recreacion de seaquest ]]
+
+function love.load()
+    local anim8 = require('lib/anim8')
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Seaquest: Definitive Edition", 200, 200)
+end
